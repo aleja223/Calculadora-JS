4fd0866d88c34e313a58d19fdeb0fc8fc3be6b9f
Aleja23 <balby223@gmail.com>
2023-10-22 21:57:22 -0500

    JS

---
+++ script.js
@@ -0,0 +1,15 @@
+//esto agrega a la pantalla el valor del boton de la calculadora que toquemos
+
+function agregar(valor) {
+  document.getElementById("pantalla").value += valor;
+}
+
+function borrar() {
+  document.getElementById("pantalla").value = "";
+}
+
+function calcular() {
+  const valorPantalla = document.getElementById("pantalla").value;
+  const resultado = eval(valorPantalla);
+  document.getElementById("pantalla").value = resultado;
+}
